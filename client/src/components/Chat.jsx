bd7ed73374e713ee456ab0a0867fda80f92e3e11
LordByron47 <55666548+LordByron47@users.noreply.github.com>
2020-08-10 11:42:17 -0700

    username color works. 1 warning, no errors

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -10,18 +10,18 @@ export class Chat extends Component {
             initialSetupComplete: false,
             allUsers: [{
                 user: props.username,
-                color: "#ffffff"
+                color: "#000000"
             }]
         }
         this.handleScroll = this.handleScroll.bind(this);
         this.determinePrevName = this.determinePrevName.bind(this);
         this.determineNextName = this.determineNextName.bind(this);
+        this.addUser = this.addUser.bind(this);
     }
 
     // scroll to the bottom of the chat when you first log in
     componentDidMount() {
         scrollTheDiv();
-
     }
 
     /* When a new chat is rendered, scroll to the bottom if either: 
@@ -29,56 +29,17 @@ export class Chat extends Component {
             2.) that chat window was already scrolled to the bottom
     */
     componentDidUpdate() {
-        let needToChangeInitialSetup = false;
         let myChats = this.props.chats;
         if (!(myChats == null) && (myChats.chats.length > 0)) {
             let last = myChats.chats[myChats.chats.length - 1];
             // if we are joining a chat with existing chats, scroll to the bottom
             if (this.state.initialSetupComplete === false) {
-                //this.setState({ initialSetupComplete: true });
-                needToChangeInitialSetup = true;
+                this.setState({ initialSetupComplete: true });
                 scrollTheDiv();
             }
             // if the logged in user sent the last chat OR was already scrolled to the bottom, scroll
             else if (last.from === this.props.username || this.state.scrolledToBottom === true) {
                 scrollTheDiv();
-            }
-            //console.log(this.state.allUsers);
-            //alert("this is a new user: "+(this.state.allUsers.includes(user => user.user === last.from) === false));
-            
-            // if the last message was from a new user, we need to assign them a color
-            //alert(JSON.stringify(this.state.allUsers));
-            /*this.state.allUsers.map(user =>{
-                alert("this user: "+user.user);
-                alert("username: "+this.props.username);
-                alert(user.user === this.props.username);
-            });
-            */
-           alert("here:" +JSON.stringify(this.state.allUsers.find(user => user.user === last.from)));
-           let newUsers = [];
-            if ((this.state.allUsers.length>0) && (last.from !== this.props.username) && (this.state.allUsers.find(user => user.user === last.from) === 'undefined')) {
-                //newUsers = [...this.state.allUsers];
-                const randColor = randomColor();
-                const toAdd = {
-                    user:last.from,
-                    color:randColor
-                };
-                newUsers = this.state.allUsers.concat(toAdd);
-                alert("new:" +newUsers);
-                //this.setState({ allUsers: newUsers });
-            }
-
-            // set the new state
-            if(needToChangeInitialSetup){
-                this.setState({
-                    initialSetupComplete:true,
-                    allUsers:newUsers
-                })
-            }
-            else{
-                this.setState({
-                    allUsers:newUsers
-                })
             }
         }
     }
@@ -115,16 +76,27 @@ export class Chat extends Component {
         }
     }
 
+    // callback function used to add a new user object to this.state.allUsers
+    addUser(newUser){
+        let newAllUsers=this.state.allUsers.concat(newUser);
+        this.setState({
+            allUsers:newAllUsers
+        });
+    }
+
+    //returns the current user object from allUsers[]. Returns undefined if not found
+    getCurrentUser(chat){
+        return this.state.allUsers.find(user => user.user === chat.from);
+    }
 
     render() {
-        //console.log(this.state.allUsers);
         let myChats = this.props.chats;
         let displayChats = null;
         // displays any messages present in chat
         if (!(myChats == null) && myChats.chats.length > 0) {
             displayChats = myChats.chats.map((chat, index) =>
-                <Message key={chat.id} contents={chat} username={this.props.username} prevName={this.determinePrevName(index)}
-                    nextName={this.determineNextName(index)} displayColor={this.state.allUsers.find(user => user.user === this.props.username).color} />
+                <Message key={chat.id} contents={chat} loggedInUsername={this.props.username} prevName={this.determinePrevName(index)}
+                    nextName={this.determineNextName(index)} currentUser={this.getCurrentUser(chat)} addUser={this.addUser} />
             );
         }
 
@@ -142,52 +114,4 @@ export default Chat;
 function scrollTheDiv() {
     let target = document.getElementById("chatDiv");
     target.scrollTop = target.scrollHeight;
-}
-
-// The below code is adapted from https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
-function randomColor() {
-    const golden_ratio_conjugate = 0.618033988749895;
-    let h = Math.random();
-    h += golden_ratio_conjugate;
-    h %= 1;
-    return hsv_to_rgb(h, 0.5, 0.95);
-}
-
-function hsv_to_rgb(h, s, v) {
-    let h_i = Number((h * 6).toFixed(0));
-    let f = h * 6 - h_i
-    let p = v * (1 - s)
-    let q = v * (1 - f * s)
-    let t = v * (1 - (1 - f) * s)
-    let r, g, b;
-    switch (h_i) {
-        case 0:
-            r = v; g = t; b = p;
-            break;
-        case 1:
-            r = q; g = v; b = p;
-            break;
-        case 2:
-            r = p; g = v; b = t;
-            break;
-        case 3:
-            r = p; g = q; b = v;
-            break;
-        case 4:
-            r = t; g = p; b = v;
-            break;
-        default:    // h_i===5
-            r = v; g = p; b = q;
-    }
-
-    return "#"+toHex((r * 256).toFixed(0)) + toHex((g * 256).toFixed(0)) + toHex((b * 256).toFixed(0));
-}
-
-// adapted from http://www.javascripter.net/faq/rgbtohex.htm
-function toHex(n) {
-    n = parseInt(n, 10);
-    if (isNaN(n)) return "00";
-    n = Math.max(0, Math.min(n, 255));
-    return "0123456789ABCDEF".charAt((n - n % 16) / 16)
-        + "0123456789ABCDEF".charAt(n % 16);
 }
--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -2,15 +2,44 @@ import React, { Component } from 'react'
 
 export class Message extends Component {
 
+    constructor(props){
+        super(props);
+        this.currentColor="";
+    }
+
+    // we get passed a user object as this.props.currentUser. We need to see if that user has a color, or, if no user was found, generate a new one
+    obtainColor(){
+        if(this.props.currentUser===undefined){
+            const newColor = randomColor();
+            const toAdd = {
+                user:this.props.contents.from,
+                color:newColor
+            };
+            this.props.addUser(toAdd);
+            this.currentColor=newColor;
+        }
+        /*
+        else{
+            return this.props.currentUser.color;
+        }
+        */
+    }
+
+    componentWillMount(){
+        //generate a color if there currently isn't one
+        this.obtainColor();
+    }
+
     render() {
         let myChat = this.props.contents;
-        let displayClass = (myChat.from === this.props.username) ? "card text-right" : "card";
+        // if the message was sent by the user logged in to this instance of the chat, we want it right-justified.
+        let displayClass = (myChat.from === this.props.loggedInUsername) ? "card text-right" : "card";
 
         let displayStyle = { borderBottomStyle: 'none' };
         let fromField = "";
         //if a sender sends multiple messages in a row, we only want the name to display for the first message
         if (this.props.prevName === myChat.from) {
-            fromField = "";
+            //fromField = "";
             displayStyle.borderTopStyle = 'none';
         }
         else {
@@ -20,12 +49,13 @@ export class Message extends Component {
         if (this.props.nextName === "") {
             displayStyle.borderBottomStyle = 'solid';
         }
-        
-        //alert(this.props.displayColor)
+
+        //logic to ensure each username is in its own color
+        //const userColor=this.obtainColor();
 
         return (
             <div className={displayClass} style={displayStyle}>
-                <dt>{fromField}</dt>
+                <dt style={{color:this.currentColor}}>{fromField}</dt>
                 <dd>{myChat.message}</dd>
             </div>
         )
@@ -33,3 +63,53 @@ export class Message extends Component {
 }
 
 export default Message
+
+
+// The below code is adapted from https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+// returns a string in format "#xxxxxx" corresponding to an RGB color value
+function randomColor() {
+    const golden_ratio_conjugate = 0.618033988749895;
+    let h = Math.random();
+    h += golden_ratio_conjugate;
+    h %= 1;
+    return hsv_to_rgb(h, 0.5, 0.95);
+}
+
+function hsv_to_rgb(h, s, v) {
+    let h_i = Number((h * 6).toFixed(0));
+    let f = h * 6 - h_i
+    let p = v * (1 - s)
+    let q = v * (1 - f * s)
+    let t = v * (1 - (1 - f) * s)
+    let r, g, b;
+    switch (h_i) {
+        case 0:
+            r = v; g = t; b = p;
+            break;
+        case 1:
+            r = q; g = v; b = p;
+            break;
+        case 2:
+            r = p; g = v; b = t;
+            break;
+        case 3:
+            r = p; g = q; b = v;
+            break;
+        case 4:
+            r = t; g = p; b = v;
+            break;
+        default:    // h_i===5
+            r = v; g = p; b = q;
+    }
+
+    return "#"+toHex((r * 256).toFixed(0)) + toHex((g * 256).toFixed(0)) + toHex((b * 256).toFixed(0));
+}
+
+// adapted from http://www.javascripter.net/faq/rgbtohex.htm
+function toHex(n) {
+    n = parseInt(n, 10);
+    if (isNaN(n)) return "00";
+    n = Math.max(0, Math.min(n, 255));
+    return "0123456789ABCDEF".charAt((n - n % 16) / 16)
+        + "0123456789ABCDEF".charAt(n % 16);
+}
